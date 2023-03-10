6794dd21c4eb7aaef5c869fea11b392694de5580
Aakrit_Bhandari <122432145+Aakrit-Bhandari@users.noreply.github.com>
2023-03-10 23:40:12 +0530

    This code will take a input of character's from the user then it will check for the character's if the letter is in captial it will convert it to lower and if the letter in lower it will convert it to upper
    Hello C Programming
    hELLO c pROGRAMMING

---
+++ upperlower/tempCodeRunnerFile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+    int l = strlen(s);
+    for (int i = 0; i < l; i++)
+    {
+        if (isupper(s[i]))
+        {
+            s[i] = tolower(s[i]);
+        }
+        else
+        {
+            s[i] = toupper(s[i]);
+        }
+    }
+    printf("%s", s);
+    return 0;
+}
